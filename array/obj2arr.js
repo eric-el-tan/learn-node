6e16a7ed7553643f9bbf1d82b4bc8826a8cc8e7f
Eric Tan <eric.tan@auckland.ac.nz>
2021-01-07 13:46:43 +1300

    add obj2arr

---
+++ array/obj2arr.js
@@ -0,0 +1,19 @@
+// var obj = { color: "red", cost: "30000", company: "Maruti" };
+
+// console.log(obj);
+
+// var arr = Object.keys(obj);
+
+// console.log(arr);
+
+// var arrObj = arr.map(function (key) {
+//   return { [key]: obj[key] };
+// });
+
+// console.log(arrObj);
+
+var budget = [{}];
+
+console.log(budget);
+budget.push({});
+console.log(budget);
