e8a87b4ef5f49e4ed0ad9a50b673c4101d636631
Eric Tan <eric.tan@auckland.ac.nz>
2021-01-14 11:36:12 +1300

    add moment test

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ etan221@it369948 MINGW64 /c/opt/08-node/learn-node (main)
 
 - `npm init -y`
 - `npm i --save-dev jest`
-- `npm test`
+- `npm test` test all
 - `node array/reduce.js`
 - `node array/obj2arr.js`, [Object To Array in Javascript](https://www.youtube.com/watch?v=Rvfl7nKNcMU)
+
+- `npm test jest/moment.test.js` - test only path/to/file
---
+++ jest/moment.test.js
@@ -0,0 +1,54 @@
+const { test, expect } = require("@jest/globals");
+const moment = require("moment");
+
+test("moment object", () => {
+  console.log(`moment: ${moment("2019-06-04 15:35:32.765")}`);
+  console.log(
+    `moment(json): ${JSON.stringify(moment("2019-06-04 15:35:32.765"))}`
+  );
+  expect(
+    moment("2019-06-04 15:35:32.765").isSame("2019-06-04 15:35:32.765")
+  ).toBe(true);
+});
+
+test("daylight saving affect date difference: Apr 06 - Apr 05: 1.041 days", () => {
+  let s1 = "2020-04-05";
+  let s2 = "2020-04-06";
+  let start = moment(s1, "YYYY-MM-DD").startOf("day");
+  let end = moment(s2, "YYYY-MM-DD").startOf("day");
+  console.log(`start: ${start} - end: ${end}`);
+  console.log(`start: ${JSON.stringify(start)} - end: ${JSON.stringify(end)}`);
+
+  let daysOfCountable = Math.abs(moment.duration(start.diff(end)).asDays());
+  console.log(`daysOfCountable: ${JSON.stringify(daysOfCountable)}`);
+
+  expect(daysOfCountable !== 1.0).toBe(true);
+});
+
+test("daylight saving affect date difference: Apr 07 - Apr 06: 1 days", () => {
+  let s1 = "2020-04-06";
+  let s2 = "2020-04-07";
+  let start = moment(s1, "YYYY-MM-DD").startOf("day");
+  let end = moment(s2, "YYYY-MM-DD").startOf("day");
+  console.log(`start: ${start} - end: ${end}`);
+  console.log(`start: ${JSON.stringify(start)} - end: ${JSON.stringify(end)}`);
+
+  let daysOfCountable = Math.abs(moment.duration(start.diff(end)).asDays());
+  console.log(`daysOfCountable: ${JSON.stringify(daysOfCountable)}`);
+
+  expect(daysOfCountable === 1.0).toBe(true);
+});
+
+test.only("remove timezone", () => {
+  let s1 = "2020-04-01";
+  let s2 = "2020-04-06"; // float number
+  let start = moment(s1, "YYYY-MM-DD").startOf("day").utc(true); // .utc(true) to remove timezone
+  let end = moment(s2, "YYYY-MM-DD").startOf("day").utc(true);
+  console.log(`start: ${start} - end: ${end}`);
+  console.log(`start: ${JSON.stringify(start)} - end: ${JSON.stringify(end)}`);
+
+  let daysOfCountable = Math.abs(moment.duration(start.diff(end)).asDays());
+  console.log(`daysOfCountable: ${JSON.stringify(daysOfCountable)}`);
+
+  expect(daysOfCountable === 5).toBe(true);
+});
