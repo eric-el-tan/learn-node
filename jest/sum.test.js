49324b3c32e0256f75c27045449a6cafd7644a49
etan221 <eric.tan@auckland.ac.nz>
2021-02-11 06:34:28 +1300

    move sum to folder

--- jest/sum.test.js
+++ jest/sum.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const sum = require('./sum')
+const sum = require('../math/sum')
 
 test('properly adds two numbers', () => {
     expect(sum(1, 2)).toBe(3)
